63a8d24f5fbcd4e53dcf1c680801e4f621a8e8c3
imadenugraha <imadenugraha@gmail.com>
2024-03-17 20:37:01 +0700

    Use extend to insert element to list

--- validateChecksum.py
+++ validateChecksum.py
@@ -6,11 +6,13 @@ def validateChecksum(md5_local, md5_s3) -> bool:
     md5_s3_list = []
     try:
         with open(md5_local, 'r') as f:
-            md5_local_list.append(f)
+            data = f.readlines()
+            md5_local_list.extend(data)
             f.close()
 
         with open(md5_s3, 'r') as f:
-            md5_s3_list.append(f)
+            data = f.readlines()
+            md5_s3_list.extend(data)
             f.close()
 
         if md5_local_list == md5_s3_list:
