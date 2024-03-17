dee37f992df26d7a75b43afc0a57bafb64616370
imadenugraha <imadenugraha@gmail.com>
2024-03-17 18:59:22 +0700

    Create MD5_S3_CHECKSUM file

--- backup.sh
+++ backup.sh
@@ -17,6 +17,7 @@ for db in $database; do
         backup_file="backup"_"$db"_"$backup_date".backup
         PGPASSWORD="$POSTGRES_PASS" pg_dump -U "$POSTGRES_USER" -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -Fc -b -v --no-owner -d "$db" &> /dev/null | sudo tee "$backup_dir/$backup_file" > /dev/null
         md5sum "$backup_dir/$backup_file" | awk '{print $1}' | tee -a ./LOCAL_MD5 > /dev/null
+        touch ./MD5_S3_CHECKSUM
         echo "Database $db berhasil dibackup ke $backup_dir/$backup_file"
     fi
 done
