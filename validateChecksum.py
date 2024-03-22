10e2bef4ee7a15546b20e2b14a2c9515628ee6fc
imadenugraha <imadenugraha@gmail.com>
2024-03-22 11:11:13 +0700

    print checksum

--- validateChecksum.py
+++ validateChecksum.py
@@ -15,8 +15,12 @@ def validateChecksum(md5_local, md5_s3) -> bool:
             md5_s3_list.extend(data)
             f.close()
 
-        if md5_local_list == md5_s3_list:
-            return True
+        for i,j in zip(md5_local_list, md5_s3_list):
+            if md5_local_list[i] == md5_s3_list[j]:
+                print(f"Local: {md5_local_list[i]} same as S3: {md5_s3_list[j]} === OK")
+            else:
+                print(f"Local: {md5_local_list[i]} is not same as S3: {md5_s3_list[j]} === OK")
+
     except Exception as e:
         logging.error(e)
         return False
